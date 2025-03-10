1533b3e6a9a3abe6aef772d8aba3c35366ebe72b
rikodemus <gregoriusricho8@gmail.com>
2025-03-10 16:01:41 +0700

    modified:   src/components/Pictures.jsx

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import backgroundImage from '../assets/dani.jpeg';
 import backgroundImage1 from '../assets/con1 (1).jpeg';
 import backgroundImage2 from '../assets/con1 (2).jpeg';
@@ -9,25 +9,36 @@ import mount3 from '../assets/mo1 (3).jpg';
 import mount4 from '../assets/mo1 (4).jpg';
 import mount5 from '../assets/mo1 (5).jpg';
 import mount6 from '../assets/mo1 (6).jpg';
+import { Link } from 'react-router-dom';
 
 const Pictures = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <nav className="bg-white border-gray-200 border-b">
         <div className="container mx-auto flex flex-wrap items-center justify-between p-4">
-          <div className="text-3xl font-bold border-4 border-black">Portofolio</div>
-          <button className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+          <div className="flex items-center">
+            <img src={backgroundImage} alt="Logo" className="h-10 w-10 mr-3 rounded-full" />
+            <div className="text-3xl font-bold">Portofolio</div>
+          </div>
+          <button
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-default"
+            aria-expanded={isOpen}
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-              <li><a href="/" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0 hover:text-xl hover:font-bold">Profile</a></li>
-              <li><a href="#lifestyle" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0 hover:text-xl hover:font-bold">Projects</a></li>
-              <li><a href="#news" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0 hover:text-xl hover:font-bold">Contact</a></li>
-              <li><a href="/pictures" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-xl font-bold md:hover:text-gray-600 md:p-0">Pictures</a></li>
+              <li><Link to="/" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 hover:text-xl hover:font-bold">Profile</Link></li>
+              <li><a href="#lifestyle" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 hover:text-xl hover:font-bold">Projects</a></li>
+              <li><a href="#news" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 hover:text-xl hover:font-bold">Contact</a></li>
+              <li><Link to="/pictures" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-xl font-bold md:hover:text-gray-600 md:p-0">Pictures</Link></li>
             </ul>
           </div>
         </div>
