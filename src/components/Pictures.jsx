496f1490b463311dde53b27e0dac95141cd6e51b
rikodemus <gregoriusricho8@gmail.com>
2025-03-10 15:40:48 +0700

    modified:   src/components/Home.jsx modified:   src/components/Pictures.jsx

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -13,14 +13,24 @@ import mount6 from '../assets/mo1 (6).jpg';
 const Pictures = () => {
   return (
     <div>
-      <nav className="flex ml-10 justify-between items-center p-4 bg-white border-gray-200 border-b">
-        <div className="text-3xl font-bold border-4 border-black">Portofolio</div>
-        <ul className="flex space-x-6 mr-10">
-          <li><a href="/" className="text-gray-800 hover:text-lg hover:font-bold">Profile</a></li>
-          <li><a href="#lifestyle" className="text-gray-800 hover:text-lg hover:font-bold">Projects</a></li>
-          <li><a href="#news" className="text-gray-800 hover:text-lg hover:font-bold">Contact</a></li>
-          <li><a href="/pictures" className="text-gray-800 text-lg  font-bold">Pictures</a></li>
-        </ul>
+      <nav className="bg-white border-gray-200 border-b">
+        <div className="container mx-auto flex flex-wrap items-center justify-between p-4">
+          <div className="text-3xl font-bold border-4 border-black">Portofolio</div>
+          <button className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+            </svg>
+          </button>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+              <li><a href="/" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Profile</a></li>
+              <li><a href="#lifestyle" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Projects</a></li>
+              <li><a href="#news" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Contact</a></li>
+              <li><a href="/pictures" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Pictures</a></li>
+            </ul>
+          </div>
+        </div>
       </nav>
 
       <div className="container mx-auto px-4 py-8 text-center text-2xl font-bold border-2 mt-5 shadow-2xl rounded-lg">  
@@ -62,7 +72,6 @@ const Pictures = () => {
                 Mountain 6
             </h1>
           </div>
-          {/* Add more photos as needed */}
         </div>
       </div>
 
@@ -70,30 +79,28 @@ const Pictures = () => {
         Exclusive Content
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10 ">
           <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={backgroundImage1} alt="Photo 1" className="w-full h-64 object-cover" />
+            <img src={backgroundImage1} alt="Photo 1" className="w-full h-48 object-cover" />
             <h1 className='font-bold text-black text-center text-lg py-2'>
                 Smile Me
             </h1>
           </div>
           <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={backgroundImage2} alt="Photo 2" className="w-full h-64 object-cover" />
+            <img src={backgroundImage2} alt="Photo 2" className="w-full h-48 object-cover" />
             <h1 className='font-bold text-black text-center text-lg py-2'>
                 Smile Me (2)
             </h1>
           </div>
           <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={backgroundImage3} alt="Photo 3" className="w-full h-64 object-cover" />
+            <img src={backgroundImage3} alt="Photo 3" className="w-full h-48 object-cover" />
             <h1 className='font-bold text-black text-center text-lg py-2'>
                 Thrifting Era
             </h1>
           </div>
-          {/* Add more photos as needed */}
         </div>
         <div className='flex justify-center'> 
-        <button className="mt-6 px-2 py-2 bg-black hover:text-black hover:bg-white  text-white font-semibold rounded-lg border-2 border-black">ORDER ME</button>
+          <button className="mt-6 px-2 py-2 bg-black hover:text-black hover:bg-white text-white font-semibold rounded-lg border-2 border-black">ORDER ME</button>
         </div>
       </div>
-
     </div>
   );
 }
