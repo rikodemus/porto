81a0640a33d16166f9875c03c893c492ac3f0208
rikodemus <gregoriusricho8@gmail.com>
2025-03-10 15:42:13 +0700

    modified:   src/components/Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,108 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import backgroundImage from '../assets/dani.jpeg';
-import backgroundImage1 from '../assets/con1 (1).jpeg';
-import backgroundImage2 from '../assets/con1 (2).jpeg';
-import backgroundImage3 from '../assets/con1 (3).jpeg';
-import mount1 from '../assets/mo1 (1).jpg';
-import mount2 from '../assets/mo1 (2).jpg';
-import mount3 from '../assets/mo1 (3).jpg';
-import mount4 from '../assets/mo1 (4).jpg';
-import mount5 from '../assets/mo1 (5).jpg';
-import mount6 from '../assets/mo1 (6).jpg';
+import { Link } from 'react-router-dom';
 
-const Pictures = () => {
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <nav className="bg-white border-gray-200 border-b">
         <div className="container mx-auto flex flex-wrap items-center justify-between p-4">
           <div className="text-3xl font-bold border-4 border-black">Portofolio</div>
-          <button className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+          <button
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-default"
+            aria-expanded={isOpen}
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-              <li><a href="/" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Profile</a></li>
+              <li><a href="#product" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Profile</a></li>
               <li><a href="#lifestyle" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Projects</a></li>
               <li><a href="#news" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Contact</a></li>
-              <li><a href="/pictures" className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Pictures</a></li>
+              <Link to="/pictures"><li><a className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-600 md:p-0">Pictures</a></li></Link>
             </ul>
           </div>
         </div>
       </nav>
 
-      <div className="container mx-auto px-4 py-8 text-center text-2xl font-bold border-2 mt-5 shadow-2xl rounded-lg">  
-        Mountain Pic's
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10">
-          <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={mount1} alt="Photo 1" className="w-full h-48 object-cover" />
-            <h1 className='font-bold text-black text-center text-lg py-2'>
-                Mountain 1
-            </h1>
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={mount2} alt="Photo 2" className="w-full h-48 object-cover" />
-            <h1 className='font-bold text-black text-center text-lg py-2'>
-                Mountain 2
-            </h1>
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={mount3} alt="Photo 3" className="w-full h-48 object-cover" />
-            <h1 className='font-bold text-black text-center text-lg py-2'>
-                Mountain 3
-            </h1>
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={mount4} alt="Photo 4" className="w-full h-48 object-cover" />
-            <h1 className='font-bold text-black text-center text-lg py-2'>
-                Mountain 4
-            </h1>
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={mount5} alt="Photo 5" className="w-full h-48 object-cover" />
-            <h1 className='font-bold text-black text-center text-lg py-2'>
-                Mountain 5
-            </h1>
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={mount6} alt="Photo 6" className="w-full h-48 object-cover" />
-            <h1 className='font-bold text-black text-center text-lg py-2'>
-                Mountain 6
-            </h1>
-          </div>
+      <section className="relative bg-cover bg-center my-5 h-screen mx-14 rounded-xl border-4 border-white" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className="absolute inset-0 bg-black opacity-50 rounded-lg"></div>
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center">
+          <h1 className="text-6xl font-bold">Portofolio</h1>
+          <p className="mt-4 max-w-xl">
+            Crafting spaces that harmonize modern aesthetics with timeless elegance, our contemporary interior designs breathe life into every room, redefining the essence of chic living.
+          </p>
+          <Link to="/pictures"> 
+          <button className="mt-6 px-6 py-3 bg-white text-black font-semibold rounded">View More</button>
+          </Link>
         </div>
-      </div>
+      </section>
 
-      <div className="container mx-auto px-4 py-8 text-center text-2xl font-bold border-2 mt-11 shadow-2xl rounded-lg mb-5 ">
-        Exclusive Content
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10 ">
-          <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={backgroundImage1} alt="Photo 1" className="w-full h-48 object-cover" />
-            <h1 className='font-bold text-black text-center text-lg py-2'>
-                Smile Me
-            </h1>
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={backgroundImage2} alt="Photo 2" className="w-full h-48 object-cover" />
-            <h1 className='font-bold text-black text-center text-lg py-2'>
-                Smile Me (2)
-            </h1>
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-lg">
-            <img src={backgroundImage3} alt="Photo 3" className="w-full h-48 object-cover" />
-            <h1 className='font-bold text-black text-center text-lg py-2'>
-                Thrifting Era
-            </h1>
+      <section className="py-12 bg-white border my-10 radius-lg">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap -mx-4">
+            <div className="w-full md:w-1/2 px-4">
+              <img src={backgroundImage} alt="Interior" className="rounded-lg border-4 border-black shadow-lg" />
+            </div>
+            <div className="w-full md:w-1/3 px-4 flex flex-col justify-center">
+              <h2 className="text-4xl font-bold mb-4 border-4 border-black">About Me</h2>
+              <p className="text-gray-700 mb-4">Currently pursuing a bachelor's degree in Information Systems, focusing on Business Intelligence at BINUS University. Passionate in the art of transforming data into insights for strategic decisions. Enthusiastic about forging connections with new individuals and embarking on novel experiences..</p>
+              <p className="text-gray-700 mb-4">Frugal Living.</p>
+              <button className="mt-6 px-6 py-3 bg-black text-white font-semibold rounded">Click For Details</button>
+            </div>
           </div>
         </div>
-        <div className='flex justify-center'> 
-          <button className="mt-6 px-2 py-2 bg-black hover:text-black hover:bg-white text-white font-semibold rounded-lg border-2 border-black">ORDER ME</button>
+      </section>
+
+      <section className="py-12 bg-gray-100">
+        <div> 
+            <h2 className="text-4xl font-bold text-center mb-5">SKILLS</h2>
         </div>
-      </div>
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap justify-center -mx-4">
+            <div className="w-full md:w-1/4 px-4 text-center">
+              <div className="text-4xl mb-4">🏆</div>
+              <h3 className="text-xl font-bold mb-2">Great Design</h3>
+              <p className="text-gray-600">Strong design skills are essential for professionals in graphic design, UI/UX design, marketing, and branding.</p>
+            </div>
+            <div className="w-full md:w-1/4 px-4 text-center">
+              <div className="text-4xl mb-4">💻</div>
+              <h3 className="text-xl font-bold mb-2">Coding Lover</h3>
+              <p className="text-gray-600">passionate about programming, problem-solving, and creating innovative solutions through code.</p>
+            </div>
+            <div className="w-full md:w-1/4 px-4 text-center">
+              <div className="text-4xl mb-4">🎯</div>
+              <h3 className="text-xl font-bold mb-2">Pixel Perfect</h3>
+              <p className="text-gray-600">delivering high-quality, visually appealing, and professional UI designs, ensuring that developers implement the design with precision.</p>
+            </div>
+            <div className="w-full md:w-1/4 px-4 text-center">
+              <div className="text-4xl mb-4">📱</div>
+              <h3 className="text-xl font-bold mb-2">Ultra Responsive</h3>
+              <p className="text-gray-600">ensures that a website or application adapts flawlessly to any screen size, device, or resolution while maintaining excellent user experience and performance.</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
 
-export default Pictures;
+export default Home;
