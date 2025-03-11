362cc677dcfbac15e5300de64242329a2bd8e798
rikodemus <gregoriusricho8@gmail.com>
2025-03-11 09:48:54 +0700

    modified:   index.html 	modified:   src/App.jsx 	new file:   src/assets/smpz8-qfevm-001.ico 	new file:   src/components/Projects.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Home from './components/Home'
 import Pictures from './components/Pictures'
+import Projects from './components/Projects'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
    <Route exact path='/' element={<Home/>} />
    <Route exact path='/pictures' element={<Pictures/>} />
+   <Route exact path='/projects' element={<Projects/>} />
         </Routes>
    </Router>
   )
