8e12db89036d4052f6990450c0c7054131416240
rikodemus <gregoriusricho8@gmail.com>
2025-03-12 09:50:10 +0700

    modified:   src/components/Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
           </button>
           <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-              <Link to="/"><li><a hclassName="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-xl font-bold md:hover:text-gray-600 md:p-0">Profile</a></li></Link>
+              <Link to="/"><li><a hclassName="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-xl font-bold md:hover:text-gray-600 md:p-0 text-xl font-bold">Profile</a></li></Link>
               <Link to="/projects"><li><a className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0 hover:text-xl hover:font-bold">Projects</a></li></Link>
               <Link to="/contact"><li><a className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0 hover:text-xl hover:font-bold">Contact</a></li></Link>
               <Link to="/pictures"><li><a className="block py-2 pl-3 pr-4 text-gray-800 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0 hover:text-xl hover:font-bold">Pictures</a></li></Link>
